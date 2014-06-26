cad69b0f896567bd3ea8f94da6d62ff5daa006bc
ashes999 <ashes999@yahoo.com>
2014-06-26 05:05:44 -0400

    Added a bunch of new scripts. Also fixed dependency loading in Auto Loader.

--- vx-ace/scripts/auto_loader.rb
+++ vx-ace/scripts/auto_loader.rb
@@ -7,6 +7,16 @@
 # Automatically loads everything in the "Scripts" subdirectory. With this,
 # we can more easily version-control our scripts.
 #========================================================================
+
+# Which scripts should you load first?
+require_first = []
+
+####
+
+require_first.each do |r|
+  require "Scripts/#{r}"
+end
+
 files = Dir.glob('Scripts/*.rb')
 files.each do |f|
     contents = File.read(f)
