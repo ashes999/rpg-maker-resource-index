5312eaa88ba5f077e1d65531a24f54997d2a25f8
ashes999 <ashes999@yahoo.com>
2016-09-09 12:43:51 -0400

    Move resources into "vxa" subdirectory

--- resources.js
+++ resources.js
@@ -2,7 +2,7 @@
 
 angular.module('app').controller('ResourcesController', ['$scope', '$http', 'resourcesRepository', 'NgTableParams', function ($scope, $http, resourcesRepo, NgTableParams) {
   // TODO: make this common across both controllers
-  this.githubRootUrl = "https://raw.githubusercontent.com/ashes999/rpg-maker-mv-resources/gh-pages";
+  this.githubRootUrl = "https://raw.githubusercontent.com/ashes999/rpg-maker-mv-resources/gh-pages/resources/vxa";
   
   this.tableParams = new NgTableParams({}, { dataset: resourcesRepo.getData() });  
 }]);
--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 angular.module('app').controller('ScriptsController', ['$scope', '$http', 'scriptsRepository', 'NgTableParams', function ($scope, $http, scriptsRepo, NgTableParams)
 {
   // TODO: make this common across both controllers
-  this.githubRootUrl = "https://raw.githubusercontent.com/ashes999/rpg-maker-mv-resources/gh-pages";
+  this.githubRootUrl = "https://raw.githubusercontent.com/ashes999/rpg-maker-mv-resources/gh-pages/resources/vxa";
   
   this.tableParams = new NgTableParams({}, { dataset: scriptsRepo.getData()});  
 }]);
