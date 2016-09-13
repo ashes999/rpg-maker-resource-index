40489b5ae793823bf085823c2f4eb7690f7c82dc
ashes999 <ashes999@yahoo.com>
2016-09-13 13:19:30 -0400

    First cut of pure angular Got rid of unnecessary JS code

--- app.js
+++ app.js
@@ -3,25 +3,28 @@
 var app = angular.module('app', ['ngTable']);
 var githubRootUrl = "https://raw.githubusercontent.com/ashes999/rpg-maker-resources/gh-pages/resources/vxa";
 
-angular.module('app').controller('ScriptsController', ['$scope', '$http', 'NgTableParams', 
+angular.module('app').controller('ResourcesController', ['$scope', '$http', 'NgTableParams', 
 function ($scope, $http, NgTableParams)
 {
   this.githubRootUrl = githubRootUrl;  
   var self = this;
 
-  $http({ method: 'GET', url: 'data/vxa/scripts.json'}).success(function(data, status, headers, config) {
-    self.tableParams = new NgTableParams({}, { dataset: data.scripts });
-  });  
+  // edition, eg. vxa, mv
+  // resourceType, eg. graphics, json (see data/vxa/graphics.json, data/vxa/scripts.json)
+  $scope.show = function(edition, resourceType)
+  {
+    console.log('show ' + edition + ' / ' + resourceType);
+    $http({ method: 'GET', url: 'data/' + edition + '/' + resourceType + '.json'}).success(function(data, status, headers, config) {
+      console.log('got data back: ' + data.resources);
+      self.tableParams = new NgTableParams({}, { dataset: data.resources });
+    });
+  }
+
 }]);
 
-angular.module('app').controller('ResourcesController', ['$scope', '$http', 'NgTableParams',
-function ($scope, $http, NgTableParams) {
-    this.githubRootUrl = githubRootUrl;
-
-  var self = this;
-
-  $http({ method: 'GET', url: 'data/vxa/graphics.json'}).success(function(data, status, headers, config) {
-    self.tableParams = new NgTableParams({}, { dataset: data.graphics });
-  });    
-}]);
-
+angular.module('app').directive('rpgResourcesTable', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'rpg-maker-resources-table.html'
+  };
+});
